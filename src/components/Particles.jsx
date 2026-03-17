eb9d4027b9cdbf199592491566b74371c28421e6
Priya Singh <priyasinghduhan@gmail.com>
2026-03-17 20:26:05 +0530

    feat: add animated particle

---
+++ src/components/Particles.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Particles.css";
+
+export default function Particles() {
+  return (
+    <div className="particles-container">
+      {Array.from({ length: 20 }).map((_, i) => (
+        <div
+          key={i}
+          className="particle"
+          style={{
+            left: `${Math.random() * 100}%`,
+            top: `${Math.random() * 100}%`,
+            animationDelay: `${Math.random() * 8}s`,
+            animationDuration: `${6 + Math.random() * 8}s`,
+            width: `${2 + Math.random() * 4}px`,
+            height: `${2 + Math.random() * 4}px`,
+          }}
+        />
+      ))}
+    </div>
+  );
+}
