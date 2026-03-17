459bd76abd737d304962d0e436cee620cb22bce4
Priya Singh <priyasinghduhan@gmail.com>
2026-03-17 21:08:56 +0530

    refactor: add component barrel exports

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Particles } from "./Particles";
+export { default as ErrorToast } from "./ErrorToast";
+export { default as TransactionTracker } from "./TransactionTracker";
+export { default as WalletModal } from "./WalletModal";
+export { default as PollOption } from "./PollOption";
+export { default as EventFeed } from "./EventFeed";
+export { default as ContractInfo } from "./ContractInfo";
+export { default as Header } from "./Header";
